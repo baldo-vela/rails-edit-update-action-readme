7ce2c70afa6925ef5ecb130a648836bd4d540cdc
baldo-vela <baldo.vela+github@gmail.com>
2021-01-19 15:15:16 -0600

    commit tree 9b9681b5ac82ed5c14084c5f149f4e17f36a2619

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def edit
+    # store the article record in an instance variable:
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
+  end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:index, :show, :new, :create]
+  
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update] # :edit & :update replace supercede 4 & 5
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article 
+  # patch 'articles/:id', to: 'articles#update'
+
 end
